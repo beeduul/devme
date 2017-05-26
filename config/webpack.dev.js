18d75e55484bce38101af685151bbf2ae2554437
Jeremy Biddle <biddle@adobe.com>
2017-05-25 22:22:08 -0700

    minimal working webpack dev env

---
+++ config/webpack.dev.js
@@ -0,0 +1,40 @@
+
+var debug = process.env.NODE_ENV !== "production";
+var webpack = require('webpack');
+const path = require('path');
+const rootDir = path.resolve(__dirname, '../');
+
+module.exports = {
+  context: __dirname,
+  
+  devtool: debug ? "inline-sourcemap" : null, // alt: 'cheap-module-eval-source-map'
+  
+  devServer: {
+      contentBase: path.resolve(rootDir, "dist"),  
+      https: true,      
+  },
+
+  entry: path.resolve(rootDir, "src/js/main.js"),
+  
+  output: {
+    path: __dirname + "/js",
+    filename: "app.min.js"
+  },
+  
+  module: {
+    rules: [
+      // babel-loader for transpling ES6 to es2015
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: [/node_modules/],
+      },
+    ],
+  },
+
+  plugins: debug ? [] : [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+  ],
+};
---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+const arrowFunction = () => {
+  const jsVersion = 'es2015 || es6';
+  return `Hello ${jsVersion}`;
+}
+module.exports = {
+  arrowFunction
+}
